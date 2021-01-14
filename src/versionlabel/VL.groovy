246ce7718fd1040f7bc4868e8ec969939248b855
nic-s1t2 <60634851+nic-s1t2@users.noreply.github.com>
2021-01-15 09:49:58 +1100

    create VL.groovy
    added scripts for managing Version Label Plugin

---
+++ src/versionlabel/VL.groovy
@@ -0,0 +1,37 @@
+#!/usr/bin/groovy
+
+package versionlabel;
+
+/* Config Location */
+def ConfigFile	= ''
+
+def RunCommand(def Command)
+{
+	bat(
+		returnStdout: true,
+		script: Command
+	)
+}
+
+def Initialise(String InProjectDir)
+{
+	ConfigFile	= "${InProjectDir}/Config/DefaultGame.ini"
+	
+	// Makes the Config File writable
+	RunCommand("attrib -r \"%ConfigFile%\"")
+}
+
+def UpdatePrefix(String NewPrefix)
+{
+	RunCommand("ini /s /Script/nfVersionLabelUI.VLSettings /i BuildNumberPrefix /v %NewPrefix% \"%ConfigFile%\"")
+}
+
+def UpdateNumber(String NewNumber)
+{
+	RunCommand("ini /s /Script/nfVersionLabelUI.VLSettings /i BuildNumber /v %NewNumber% \"%ConfigFile%\"")
+}
+
+def UpdateSuffix(String NewSuffix)
+{
+	RunCommand("ini /s /Script/nfVersionLabelUI.VLSettings /i BuildNumberSuffix /v \"_%NewSuffix%\" \"%ConfigFile%\"")
+}
