caf656f49fcb581ca3a55dd76c41854890b1006e
nic-s1t2 <60634851+nic-s1t2@users.noreply.github.com>
2021-02-17 13:51:04 +1100

    oop

--- src/misc/Misc.groovy
+++ src/misc/Misc.groovy
@@ -26,7 +26,7 @@ def BuildVsProject(String vsProjPath, String configuration)
 {
 	if(isUnix())
 	{
-		RunCommand("/usr/bin/local/msbuild \"${vsProjPath}\" -t:rebuild -r -p:\"Configuration=${configuration}\"")
+		RunCommand("/usr/local/bin/msbuild \"${vsProjPath}\" -t:rebuild -r -p:\"Configuration=${configuration}\"")
 	}
 	else
 	{
